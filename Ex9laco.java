593dd0f2daaa61e4c0efa42bb9cf17722a48779d
Erick71283510 <160851054+Erick71283510@users.noreply.github.com>
2024-04-15 09:13:59 -0300

    Add files via upload

---
+++ Ex9laco.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex9laco;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Ex9laco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 101; 
+        int contador = 0;
+
+        
+        while (contador < 10) {
+            System.out.println(numero);
+            numero++; 
+            contador++;         
+        }
+    }
+    
+}
